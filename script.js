567e518721f46a397040c45f3d6057083c25dd85
Anand Hadli <anandhadli80@gmail.com>
2025-11-20 18:51:17 +0530

    Create a grid of boxes

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 let container = document.querySelector(".container");
+let k = 10;
 
-container.setAttribute(
-  "style",
-  "display: flex; flex-direction: column; gap: 20px;"
-);
+container.setAttribute("style", "display: flex; flex-direction: column;");
 
 function generateBox() {
-  for (let i = 1; i <= 2; i++) {
+  for (let i = 1; i <= k; i++) {
     let boxColumn = document.createElement("div");
     boxColumn.classList.add("boxColumn");
 
     boxColumn.setAttribute(
       "style",
-      "display: flex; flex-direction: row; padding: 20px; border: 2px solid red;"
+      "display: flex; flex-direction: row; ; border: 2px solid red; border-radius: 5px;"
     );
 
     container.appendChild(boxColumn);
 
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 1; i <= k; i++) {
       let box = document.createElement("div");
       box.classList.add("box");
       box.setAttribute(
         "style",
-        "background-color: green; padding: 30px; border: 2px solid blue;"
+        "background-color: green; max-width: 50px; max-height: 50px; width: 50px; height: 50px; border: 2px solid blue; border-radius: 5px;"
       );
       boxColumn.append(box);
-      console.log("This ran");
     }
   }
 }
 
+function draw() {
+  const box = document.querySelector(".box");
+  box.addEventListener("click", (event) => {
+    box.setAttribute("style", "background-color: black");
+    console.log("this ran two");
+  });
+  console.log("This ran three");
+}
+
 generateBox();
+draw();
