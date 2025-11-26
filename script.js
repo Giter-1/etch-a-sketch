1c5243c2539fe1ed595b640fadcf385e78f50e5c
Anand Hadli <anandhadli80@gmail.com>
2025-11-26 11:57:28 +0530

    Fit boxes to grid

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ document.body.onclick = () => (clicky = true);
 
 container.style.display = "flex";
 container.style.flexDirection = "column";
-container.style.width = "960px";
+container.style.border = "2px solid red";
+container.style.width = "1200px";
+container.style.height = "1200px";
 
 function generateBox() {
   for (let i = 1; i <= k; i++) {
@@ -18,53 +20,37 @@ function generateBox() {
 
     boxColumn.style.display = "flex";
     boxColumn.style.flexDirection = "row";
-    boxColumn.style.borderRadius = "5px";
 
     container.appendChild(boxColumn);
 
     for (let j = 1; j <= k; j++) {
       let box = document.createElement("div");
       box.classList.add("box");
-      let size = String(int(960 / k)) + "px";
+      let size = String(1200 / k) + "px";
       console.log(size);
 
       box.style.backgroundColor = "white";
-      box.style.maxWidth = "50px";
-      box.style.maxHeight = "50px";
-      box.style.width = "50px";
-      box.style.height = "50px";
-      box.style.border = "1px solid blue";
+      // box.style.maxWidth = size;
+      // box.style.maxHeight = size;
+      box.style.width = size;
+      box.style.height = size;
+      // box.style.border = "1px solid blue";
 
       boxColumn.append(box);
     }
   }
 }
-
 function draw() {
   const box = Array.from(document.querySelectorAll(".box"));
 
   box.forEach((b) => {
     b.addEventListener("mouseover", (event) => {
-      console.log(clicky);
-      console.log(mouseDown);
       if (clicky && mouseDown) {
         b.style.backgroundColor = "black";
       }
-      console.log("this ran two");
     });
   });
-
-  console.log("This ran three");
 }
 
 generateBox();
 draw();
-
-// box.forEach((b) => {
-//   b.addEventListener("mouseover", (event) => {
-//     b.addEventListener("click", (event) => {
-//       b.style.backgroundColor = "black";
-//       console.log("this ran two");
-//     });
-//   });
-// });
